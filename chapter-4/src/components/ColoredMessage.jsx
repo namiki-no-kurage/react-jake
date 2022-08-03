a1ffd1d9a2fba2fad576aba876a7fca7572187a1
namiki-no-kurage <m.hirayama0129+namiki@gmail.com>
2022-08-04 00:44:36 +0900

    [update]childrenによる値渡し

--- chapter-4/src/components/ColoredMessage.jsx
+++ chapter-4/src/components/ColoredMessage.jsx
@@ -1,10 +1,10 @@
 export const ColoredMessage = (props) => {
-  const { color, message } = props
+  const { color, children } = props
 
   const contentStyle = {
     color: color,
     fontSize: '20px'
   }
 
-  return <p style={ contentStyle }>{ message }</p>
+  return <p style={ contentStyle }>{ children }</p>
 }
