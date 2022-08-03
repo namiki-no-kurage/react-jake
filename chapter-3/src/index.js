5b650935276b20aa2625f054bebe39efcc2d2dca
namiki-no-kurage <m.hirayama0129+namiki@gmail.com>
2022-08-03 23:52:46 +0900

    [update]簡単なメモアプリの雛形に上書き

--- chapter-3/src/index.js
+++ chapter-3/src/index.js
@@ -1,11 +1,1 @@
-const title1 = document.getElementById("title")
-console.log(title1)
-
-const title2 = document.querySelector("#title")
-console.log(title2)
-
-const containers = document.getElementsByClassName("container")
-console.log(containers)
-
-const container = document.querySelector(".container")
-console.log(container)
+console.log('test')
