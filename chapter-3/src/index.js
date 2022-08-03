b8c37dd4cba05e648b2823fbe69bd3da978b4929
namiki-no-kurage <m.hirayama0129+namiki@gmail.com>
2022-08-03 23:45:47 +0900

    [update]JSによるDOM取得

--- chapter-3/src/index.js
+++ chapter-3/src/index.js
@@ -1,1 +1,11 @@
-console.log('test')
+const title1 = document.getElementById("title")
+console.log(title1)
+
+const title2 = document.querySelector("#title")
+console.log(title2)
+
+const containers = document.getElementsByClassName("container")
+console.log(containers)
+
+const container = document.querySelector(".container")
+console.log(container)
